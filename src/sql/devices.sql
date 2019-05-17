3ef0fcfccfe12789bdfb77a699950f41ab49b9c8
Florian <florian.schmidt.welzow@t-online.de>
2019-05-17 13:04:53 +0200

    Each user can have up to one device only

--- src/sql/devices.sql
+++ src/sql/devices.sql
@@ -6,5 +6,6 @@ CREATE TABLE /*$wgDBprefix*/passwordlesslogin_devices (
   device_id varchar(255) NULL,
   device_user_id int(10) unsigned NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY (device_id)
+  UNIQUE KEY (device_id),
+  UNIQUE KEY (device_user_id)
 ) /*$wgDBTableOptions*/;
