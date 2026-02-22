de305a73e12950428cca500a374cfea102e0b272
Fomafix <fomafix@googlemail.com>
2026-02-22 19:59:05 +0000

    ESLint: Use server config for server files
    Fix errors and warnings reported by ESLint.
    
    Also ignore files using .eslintignore.
    
    Change-Id: Ife818c6da96689461c44cc0eedaca835bf698622

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,11 @@
-/* eslint-env node, es6 */
+'use strict';
 module.exports = function ( grunt ) {
-	var conf = grunt.file.readJSON( 'extension.json' );
+	const conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 
 	grunt.initConfig( {
-		// eslint-disable-next-line es/no-object-assign, compat/compat
 		banana: Object.assign(
 			conf.MessagesDirs,
 			{
@@ -19,11 +18,7 @@ module.exports = function ( grunt ) {
 			options: {
 				cache: true
 			},
-			all: [
-				'**/*.json',
-				'!node_modules/**',
-				'!vendor/**'
-			]
+			all: [ '.' ]
 		}
 	} );
 
--- ui/login.js
+++ ui/login.js
@@ -1,35 +1,36 @@
 ( function () {
-	var api = new mw.Api();
-	var form = $("[name='userlogin']");
+	const api = new mw.Api();
+	// eslint-disable-next-line no-jquery/no-global-selector
+	const $form = $( "[name='userlogin']" );
 
 	function submitForm() {
-		form.submit();
+		$form.trigger( 'submit' );
 	}
 
 	function pendingVerification() {
-		api.get({
+		api.get( {
 			action: 'passwordlesslogin-verify-login'
-		}).done(function(data) {
-			if (data['login-verification']['result'] === 'Success') {
-				$('<div/>')
-					.css('z-index', '1')
-					.css('height', '100%')
-					.css('width', '100%')
-					.css('position', 'absolute')
-					.insertBefore(form);
-				form.css('opacity', '0.5');
+		} ).then( ( data ) => {
+			if ( data[ 'login-verification' ].result === 'Success' ) {
+				$( '<div>' )
+					.css( 'z-index', '1' )
+					.css( 'height', '100%' )
+					.css( 'width', '100%' )
+					.css( 'position', 'absolute' )
+					.insertBefore( $form );
+				$form.css( 'opacity', '0.5' );
 				submitForm();
 			} else {
 				pendingVerification();
 			}
-		});
+		} );
 	}
 
-	if (mw.config.get('PLEnableApiVerification')) {
+	if ( mw.config.get( 'PLEnableApiVerification' ) ) {
 		pendingVerification();
 	} else {
-		setTimeout(function () {
+		setTimeout( () => {
 			submitForm();
-		}, 3000)
+		}, 3000 );
 	}
 }() );
